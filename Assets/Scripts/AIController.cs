7c5e1a5f12f5b67faac86acd5060041793475ca5
Rachel <rachiecool@hotmail.com>
2023-04-06 19:47:44 +0100

    added wonder function

--- Assets/Scripts/AIController.cs
+++ Assets/Scripts/AIController.cs
@@ -6,6 +6,8 @@ using UnityEngine.AI;
 public class AIController : MonoBehaviour
 {
     private NavMeshAgent _navMeshAgent;
+    public float timer, wanderTimer;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -24,10 +26,25 @@ public class AIController : MonoBehaviour
             }
          
         }
+
+        timer += Time.deltaTime;
+        Wander();
     }
 
     private void SetAITargetLocation(Vector3 targetLocation)
     {
         _navMeshAgent.SetDestination(targetLocation);
+    }
+
+    private void Wander()
+    {
+        if(timer >= wanderTimer)
+        {
+            Vector2 wanderTarget = Random.insideUnitCircle * wanderTimer;
+            Vector3 wanderPos3D = new Vector3(transform.position.x + wanderTarget.x, transform.position.y, transform.position.z + wanderTarget.y);
+            SetAITargetLocation(wanderPos3D);
+            timer = 0;
+        }
+
     }
 }
